644c0facb466e41611aa73a30c9fdf7e4cf7308e
alina.fedyniak <a.fedynyak@artjoker.team>
2023-02-21 00:15:40 +0200

    add i18next, add config, app file fix, fix index

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  padding: 5rem;
+  text-align: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <StyledWrapper>
+            <h1>My App</h1>
+        </StyledWrapper>
+    );
 }
 
 export default App;
